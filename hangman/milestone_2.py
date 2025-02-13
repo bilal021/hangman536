ee4f426156a82846759cb28eb84d47e5bce76619
Bilal <bilal_021@live.com>
2025-02-13 16:22:39 +0000

    Refactored code for better readability and meaningful variable naming

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -4,17 +4,14 @@ import random
 
 
 word_list = ['Apple', 'Banana', 'Blueberry', 'Kiwi', 'Strawberry']
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-word = random.choice(word_list)
+# Ask the user to enter a single word
+user_guess = input('Entre your guess: ')
 
-guess = input('Entre your guess: ')
-
+# Validate the input
 if len(guess) == 1 and guess.isalpha():
     print('Good guess!')
 else:
     print('Oops! That is not a valid input.')
-
-
-print(guess)
-
-#print(word_list)
