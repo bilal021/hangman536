74d83dc48d90183b77b1640a3a9d14d1f05d6f65
Bilal <bilal_021@live.com>
2025-02-13 15:55:35 +0000

    Added user input handling and validation for letter guess

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,5 +1,20 @@
 # Hangman: Milestone 2
 
+import random 
+
+
 word_list = ['Apple', 'Banana', 'Blueberry', 'Kiwi', 'Strawberry']
 
-print(word_list)
+word = random.choice(word_list)
+
+guess = input('Entre your guess: ')
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
+
+
+print(guess)
+
+#print(word_list)
